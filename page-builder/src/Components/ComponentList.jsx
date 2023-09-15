2cf533922d85988c3393f728cbbe49895056b18f
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2023-09-15 06:33:05 +0000

    feat: dragging component is working

--- page-builder/src/Components/ComponentList.jsx
+++ page-builder/src/Components/ComponentList.jsx
@@ -2,6 +2,7 @@ import react from 'react';
 import { useDispatch } from "react-redux";
 import { v4 as uuid } from 'uuid';
 import { VscAdd } from "react-icons/vsc";
+import { useDrag } from 'react-dnd'
 import { components } from "../Data/Components";
 import { addLayers, addSection, activeSection } from "../Redux/Actions/BuildActions";
 
@@ -19,25 +20,33 @@ function ComponentList(){
         <div className='component-list'>
             <div className='component-list-header'>Components</div>
             <div className='component-list-column'>
-                {
-                    components.map(item =>{
-                        return(
-                            <div className='component-container d-flex justify-content-between' key={item.id}>
-                                <div className='left-side-component-container d-flex'>
-                                    <div className='component-container-icon px-2'>{item.icon}</div>
-                                    <div className='component-container-label'>{item.component}</div>
-                                </div>
-                                <div className='right-side-component-container cursor-pointer' 
-                                    onClick={() => handleComponentAdd(item.layer, uuid())}>
-                                    <VscAdd/>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
+                {components.map(item => <Component key={item.id} handleComponentAdd={handleComponentAdd} item={item}/>)}
             </div>
         </div>
     )
 }
 
 export default ComponentList;
+
+
+
+function Component({item, handleComponentAdd}){
+    const [{isDragging}, drag] = useDrag(() => ({
+        type: item.component,
+        collect: monitor => ({
+          isDragging: !!monitor.isDragging(),
+        }),
+      }))
+    return(
+        <div className='component-container d-flex justify-content-between' ref={drag}>
+            <div className='left-side-component-container d-flex'>
+                <div className='component-container-icon px-2'>{item.icon}</div>
+                <div className='component-container-label'>{item.component}</div>
+            </div>
+            <div className='right-side-component-container cursor-pointer' 
+                onClick={() => handleComponentAdd(item.layer, uuid())}>
+                <VscAdd/>
+            </div>
+        </div>
+    )
+}
