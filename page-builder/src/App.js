9c9f762587af59986d2c24bdfcf478956804f94e
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2023-09-15 06:22:03 +0000

    feat: Added React DnD in the project

--- page-builder/src/App.js
+++ page-builder/src/App.js
@@ -1,14 +1,16 @@
 import { RouterProvider } from "react-router-dom";
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 import Router from "./Routes";
 import Header from "./Home/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App(){
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Header/>
       <Router />
-    </>
+    </DndProvider>
   )
 }
 
